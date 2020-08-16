c5054e82a82a640dd4a5581fe2380fbfbe9d1034
Dalton Menezes <daltonmenezes@outlook.com>
2020-08-16 17:29:08 -0300

    Add Aura Dark Plus Soft Text variant

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,7 @@
 ## 1.1.0 (August 16, 2020)
 
 - 🎉 Add Aura Dark Soft Text variant
+- 🎉 Add Aura Dark Plus Soft Text variant
 
 ## 1.0.0 (August 16, 2020)
 
--- src/themes/index.js
+++ src/themes/index.js
@@ -2,5 +2,6 @@ module.exports = [
   require('./aura-dark'),
   require('./aura-dark-soft-text'),
   require('./aura-dark-plus'),
+  require('./aura-dark-plus-soft-text'),
   require('./aura-soft-dark'),
 ]
