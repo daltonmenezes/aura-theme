1caecbae2c5765e28a6630c616f78c7910a3b508
Daniel K <dklaver15@gmail.com>
2022-04-18 12:32:07 -0500

    Update index.ts
    Removed code that was added during troubleshooting

--- src/services/file-system/features/copy-files/providers/cpy/index.ts
+++ src/services/file-system/features/copy-files/providers/cpy/index.ts
@@ -1,6 +1,5 @@
 import { CopyFilesServiceProvider } from 'core/@types/services/file-system'
 import copy from 'cpy'
-import fetch from 'node-fetch'
 
 export function cpyProvider(): CopyFilesServiceProvider {
   return {
