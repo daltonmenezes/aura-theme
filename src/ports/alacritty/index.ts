b8b13c3cf2f2149d11ebc5b07d2155410c6965f6
Dalton Menezes <daltonmenezes@outlook.com>
2022-08-11 23:50:50 -0300

    chore(alacritty): up version to 1.0.1

--- src/ports/alacritty/index.ts
+++ src/ports/alacritty/index.ts
@@ -6,7 +6,7 @@ export async function AlacrittyPort(Aura: AuraAPI) {
   const { info } = constants
 
   const portName = 'Alacritty terminal'
-  const version = '1.0.0'
+  const version = '1.0.1'
   const previewURL = `https://github.com/${info.author.username}/assets/blob/master/images/${info.slug}/aura-alacritty-preview.png?raw=true`
   const templateFolder = resolve(__dirname, 'templates')
 
