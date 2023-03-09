57e2d404e58d898ab60670820b517308386278e0
Hendrikto <hendrik.to@gmail.com>
2023-03-09 01:19:24 +0100

    feat(port): set preview URL

--- src/ports/waybar/index.ts
+++ src/ports/waybar/index.ts
@@ -8,6 +8,7 @@ export async function WaybarPort(Aura: AuraAPI) {
 
   const portName = 'Waybar'
   const version = '1.0.0'
+  const previewURL = `https://user-images.githubusercontent.com/1149845/223881256-6726819c-5ce5-4c84-a84b-0a4f70993b96.png`
 
   await createPort({
     template: resolve(templateFolder, `style.css`),
@@ -22,6 +23,7 @@ export async function WaybarPort(Aura: AuraAPI) {
     replacements: {
       portName,
       version,
+      previewURL,
     },
   })
 }
